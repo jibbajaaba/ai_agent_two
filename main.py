1ad1bc094c7d64dccc6fbc5c1976e28af562911e
Mark A McTague <mmctaguecoding@gmail.com>
2026-02-05 22:05:17 -0500

    created a loop for the agent to call the generate function 20 times

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import argparse
+import sys
 
 from dotenv import load_dotenv
 from google import genai
@@ -21,9 +22,38 @@ def main():
 
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    generate_content(client, messages, args)
+
+    # loop for agent to make function calls
+    for _ in range(20):
+        response = generate_content(client, messages, args)
+
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+
+        if response.function_calls:
+            function_response_parts = []
+            for function_call in response.function_calls:
+                function_response = call_function(function_call, args.verbose)
+                if function_response.parts == []:
+                    raise Exception("parts should not be an empty list")
+                part = function_response.parts[0]
+
+                function_response_parts.append(part)
+
+            messages.append(types.Content(role="user", parts=function_response_parts))
+
+            continue
+
+        print("Final Response")
+        print(response.text)
+        return
+
+    print("Agent failed to produce a final reponse")
+    sys.exit(1)
 
 
+# function to generate the content for agent to use
 def generate_content(client, messages, args):
     response = client.models.generate_content(
         model='gemini-2.5-flash',
@@ -35,34 +65,36 @@ def generate_content(client, messages, args):
             )
     )
 
-    if not response.usage_metadata:
-        raise RuntimeError("Gemini API response appears to be malformed")
+    # if not response.usage_metadata:
+    #     raise RuntimeError("Gemini API response appears to be malformed")
 
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    candidates_tokens = response.usage_metadata.candidates_token_count
+    return response
 
-    if args.verbose:
-        print("User prompt:", args.user_prompt)
-        print("Prompt tokens:", prompt_tokens)
-        print("Response tokens:", candidates_tokens)
-    if response.function_calls:
-        function_result_list = []
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call, args.verbose)
-            if function_call_result.parts == []:
-                raise Exception("parts should not be an empty list")
-            if function_call_result.parts[0].function_response is None:
-                raise Exception("function response in parts should not be None")
-            if function_call_result.parts[0].function_response.response is None:
-                raise Exception("response in function response should not be None.")
-            if args.verbose:
-                print(
-                    f"-> {function_call_result.parts[0].function_response.response}"
-                )
-            function_result_list.append(function_call_result.parts[0])
-    else:
-        print("Response:")
-        print(response.text)
+    # prompt_tokens = response.usage_metadata.prompt_token_count
+    # candidates_tokens = response.usage_metadata.candidates_token_count
+
+    # if args.verbose:
+    #     print("User prompt:", args.user_prompt)
+    #     print("Prompt tokens:", prompt_tokens)
+    #     print("Response tokens:", candidates_tokens)
+    # if response.function_calls:
+    #     function_result_list = []
+    #     for function_call in response.function_calls:
+    #         function_call_result = call_function(function_call, args.verbose)
+    #         if function_call_result.parts == []:
+    #             raise Exception("parts should not be an empty list")
+    #         if function_call_result.parts[0].function_response is None:
+    #             raise Exception("function response in parts should not be None")
+    #         if function_call_result.parts[0].function_response.response is None:
+    #             raise Exception("response in function response should not be None.")
+    #         if args.verbose:
+    #             print(
+    #                 f"-> {function_call_result.parts[0].function_response.response}"
+    #             )
+    #         function_result_list.append(function_call_result.parts[0])
+    # else:
+    #     print("Response:")
+    #     print(response.text)
 
 
 if __name__ == "__main__":
